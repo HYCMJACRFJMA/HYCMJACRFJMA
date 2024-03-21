2a338b377c3956481237e73982cea6f346af0174
Hillary Yeraldy Carpio Morales <yeraldycarpio_morales@hotmail.com>
2024-03-21 11:14:00 -0600

    controladores

---
+++ FJMAJACRHYCM/Controllers/AddressesController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FJMAJACRHYCM.Models;
+
+namespace FJMAJACRHYCM.Controllers
+{
+    public class AddressesController : Controller
+    {
+        private readonly HYCMJACRJFMA_DBContext _context;
+
+        public AddressesController(HYCMJACRJFMA_DBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Addresses
+        public async Task<IActionResult> Index()
+        {
+            var hYCMJACRJFMA_DBContext = _context.Addresses.Include(a => a.Employee);
+            return View(await hYCMJACRJFMA_DBContext.ToListAsync());
+        }
+
+        // GET: Addresses/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Addresses == null)
+            {
+                return NotFound();
+            }
+
+            var address = await _context.Addresses
+                .Include(a => a.Employee)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            return View(address);
+        }
+
+        // GET: Addresses/Create
+        public IActionResult Create()
+        {
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "Id");
+            return View();
+        }
+
+        // POST: Addresses/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,EmployeeId,Street,City,State,Country,PostalCode")] Address address)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(address);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "Id", address.EmployeeId);
+            return View(address);
+        }
+
+        // GET: Addresses/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Addresses == null)
+            {
+                return NotFound();
+            }
+
+            var address = await _context.Addresses.FindAsync(id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "Id", address.EmployeeId);
+            return View(address);
+        }
+
+        // POST: Addresses/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,EmployeeId,Street,City,State,Country,PostalCode")] Address address)
+        {
+            if (id != address.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(address);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AddressExists(address.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "Id", address.EmployeeId);
+            return View(address);
+        }
+
+        // GET: Addresses/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Addresses == null)
+            {
+                return NotFound();
+            }
+
+            var address = await _context.Addresses
+                .Include(a => a.Employee)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            return View(address);
+        }
+
+        // POST: Addresses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Addresses == null)
+            {
+                return Problem("Entity set 'HYCMJACRJFMA_DBContext.Addresses'  is null.");
+            }
+            var address = await _context.Addresses.FindAsync(id);
+            if (address != null)
+            {
+                _context.Addresses.Remove(address);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AddressExists(int id)
+        {
+          return (_context.Addresses?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ FJMAJACRHYCM/Controllers/EmployesController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FJMAJACRHYCM.Models;
+
+namespace FJMAJACRHYCM.Controllers
+{
+    public class EmployesController : Controller
+    {
+        private readonly HYCMJACRJFMA_DBContext _context;
+
+        public EmployesController(HYCMJACRJFMA_DBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Employes
+        public async Task<IActionResult> Index()
+        {
+              return _context.Employees != null ? 
+                          View(await _context.Employees.ToListAsync()) :
+                          Problem("Entity set 'HYCMJACRJFMA_DBContext.Employees'  is null.");
+        }
+
+        // GET: Employes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Employees == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Email,Phone")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(employee);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        // GET: Employes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Employees == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Email,Phone")] Employee employee)
+        {
+            if (id != employee.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(employee);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmployeeExists(employee.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        // GET: Employes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Employees == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // POST: Employes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Employees == null)
+            {
+                return Problem("Entity set 'HYCMJACRJFMA_DBContext.Employees'  is null.");
+            }
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee != null)
+            {
+                _context.Employees.Remove(employee);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmployeeExists(int id)
+        {
+          return (_context.Employees?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
